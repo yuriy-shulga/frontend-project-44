0f35f4b5f5c1f8d10b6eae45594db668a28eb9be
Yuriy Shulga <yshulgag@gmail.com>
2024-08-31 00:21:08 +0300

    Fix: move the user greeting function from the entry point

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import app from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${name}!`);
+app();
